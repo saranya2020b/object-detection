74b84799654739adb517c597655408275fbb80f8
saranya2020b <81304177+saranya2020b@users.noreply.github.com>
2022-03-15 15:35:08 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,44 @@
+Status = "";
+bulb_image = "";
+objects = [];
+
+function preload(){
+    bulb_image = loadImage("https://media.istockphoto.com/photos/an-old-incandescent-bulb-picture-id1170694997?k=20&m=1170694997&s=612x612&w=0&h=jmza0K7FRi3ua81-qaPw-9OqyljPO6brqZ-T1-3VU_k=");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(bulb_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(bulb_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 14, objects[i].y - 175);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);
+        }
+    }
+}
